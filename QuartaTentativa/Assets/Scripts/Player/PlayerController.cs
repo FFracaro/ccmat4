5f207e8b22687fd076306f6bc270bd79b675a5a5
ffracaro <frafracaro@gmail.com>
2018-10-20 22:53:07 -0300

    Movimento em 8 direções, sem animação de frente e costas

---
+++ QuartaTentativa/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject Player;
+    public float Speed = 0f;
+
+    private Rigidbody2D PlayerBody;
+    private Animator Animador;
+    private bool Right = true;
+    private SpriteRenderer Renderer;
+    private float moveX, moveY;
+
+	// Use this for initialization
+	void Start ()
+    {
+        PlayerBody = Player.GetComponent<Rigidbody2D>();
+        Animador = Player.GetComponent<Animator>();
+        Renderer = Player.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+
+        Vector3 VectorXY = new Vector3(moveX, moveY, 0);
+        VectorXY = VectorXY.normalized * Speed * Time.deltaTime;
+        PlayerBody.MovePosition(PlayerBody.transform.position + VectorXY);
+
+        //PlayerBody.velocity = new Vector2(moveX * Speed, PlayerBody.velocity.y);
+
+        Animador.SetFloat("Speed", Mathf.Abs(moveX));
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (Renderer.flipX)
+            {
+                Renderer.flipX = false;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (!Renderer.flipX)
+            {
+                Renderer.flipX = true;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            // Subindo
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            // frente do personagem
+        }
+    }
+}
