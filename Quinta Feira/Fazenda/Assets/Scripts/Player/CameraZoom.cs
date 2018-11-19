1398ac2a7810c3000bb8188569731b279434e4e7
FFracaro <35259802+FFracaro@users.noreply.github.com>
2018-11-19 19:11:59 -0200

    Vamo que vamo
    Reconstrução da poha toda em 3D.

---
+++ Quinta Feira/Fazenda/Assets/Scripts/Player/CameraZoom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (Camera.main.fieldOfView <= 90)
+                Camera.main.fieldOfView += 2;
+        }
+        
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (Camera.main.fieldOfView > 10)
+                Camera.main.fieldOfView -= 2;
+        }
+	}
+}
---
+++ Quinta Feira/Fazenda/Assets/Scripts/Sounds/Ambiente/MusicLoop.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicLoop : MonoBehaviour
+{
+    public AudioClip[] audioBG;
+    private AudioSource bgMusic1, bgMusic2, bgMusic3, AudioSource;
+
+    private void Start()
+    {
+        AudioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!bgMusic3.isPlaying && !bgMusic1.isPlaying)
+        {
+            AudioSource.clip = audioBG[1];
+            AudioSource.Play();
+        } 
+        if (!bgMusic3.isPlaying && !bgMusic2.isPlaying)
+        {
+            AudioSource.clip = audioBG[0];
+            AudioSource.Play();
+        }
+            
+        if (!bgMusic1.isPlaying && !bgMusic2.isPlaying)
+        {
+            AudioSource.clip = audioBG[2];
+            AudioSource.Play();
+        }
+    }
+}
